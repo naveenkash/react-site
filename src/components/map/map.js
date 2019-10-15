6bcae2fac039b65b085e3e4fc4e4478a9dff614a
Naveen Kashyap <naveenkashyap243@gmail.com>
2019-10-16 00:03:27 +0530

    UPDATE map image

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -10,7 +10,7 @@ function map() {
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt recusandae vitae placeat ullam nulla iusto cum enim numquam voluptates unde illum commodi aperiam quisquam similique quia optio qui impedit tempore, necessitatibus ad beatae? Exercitationem, esse dolor! Aperiam quaerat nostrum, qui possimus sequi omnis esse ex, adipisci itaque, porro temporibus magni.</p>
                     </div>
                     <div className="map_img">
-                        <img src="https://library.kissclipart.com/20180904/guq/kissclipart-world-map-vector-japan-center-clipart-world-map-df8e990a72d499bc.jpg" alt=""/>
+                        <img src="https://static.vecteezy.com/system/resources/thumbnails/000/376/072/small/Global_Network_3.jpg" alt=""/>
                     </div>
                 </div>
             </div>
